2ea3c6a5560f222e70f9500f92cc82004474cd89
SharjAhmed <sharj.ahmed.91@gmail.com>
2023-03-28 00:07:53 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+
+board = []
+
+for x in range(0, 5):
+    board.append(["o"]*5)
+
+
+def print_board(board):
+    for row in board:
+        print("|".join(row))
+
+print_board(board)
