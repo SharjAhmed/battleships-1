f0a1270876316c91d0decbbf1c3fe1fa305774fd
SharjAhmed <sharj.ahmed.91@gmail.com>
2023-03-30 22:34:11 +0000

    Updated board to show row and column headings

--- run.py
+++ run.py
@@ -2,12 +2,19 @@ from random import randint
 
 board = []
 
-for x in range(0, 5):
-    board.append(["o"]*5)
+for x in range(0, 8):
+    board.append(["▢"]*8)
 
 
 def print_board(board):
+    '''
+    Create board to see where user can guess where to hit
+    '''
+    print('    A   B   C   D   E   F   G   H')
+    print('  --------------------------------')
+    row_number = 1
     for row in board:
-        print(" ".join(row))
+        print('%d | %s | ' % (row_number, ' | '.join(row)))
+        row_number += 1
 
 print_board(board)
