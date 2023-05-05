def57008dd0bad84ee24be1dbe3e191b1ea7a219
SharjAhmed <sharj.ahmed.91@gmail.com>
2023-05-05 23:02:36 +0000

    Updated fire_shot function to validate for blank input from user

--- run.py
+++ run.py
@@ -3,16 +3,11 @@ Import random to generate random integers in game
 """
 from random import randint
 
-# Game Legend
-# ▢ - blank space
-# ⨀ - missed shot
-# ⊗ - hit ship
 
 HIDDEN_BOARD = [["▢"] * 8 for x in range(8)]
 GUESS_BOARD = [["▢"] * 8 for x in range(8)]
 letters_to_numbers = {
-    "A": 0,1
-    yyyy
+    "A": 0,
     "B": 1,
     "C": 2,
     "D": 3,
@@ -36,8 +31,13 @@ def print_board(board):
         row_number += 1
 
 
-print_board(GUESS_BOARD)
-print_board(HIDDEN_BOARD)
+print("\n⛴  WELCOME TO BATTLESHIPS ⛴\n")
+print("YOU HAVE 25 GUESSES TO SINK 5 BATTLESHIPS\n")
+print("Game Legend:")
+print("▢ - blank space")
+print("⊗ - missed shot")
+print("♨ - hit ship\n")
+print_board(board)
 
 
 def create_ships(board):
@@ -55,15 +55,21 @@ def fire_shot():
     """
     Asking player what row and column to fire their shot
     """
-    row = input("\nEnter ship row 1-8: ")
+
+    row = input("\nEnter ship row 1-8: ").upper()
+    while len(row) == 0:
+        row = input("\nEnter ship row 1-8: ").upper()
     while row not in "12345678":
-        print("\nPlease enter a valid row")
-        row = input("\nEnter ship row 1-8: \n")
+        print('Please select a valid row 1 to 8')
+        row = input("Ship Row: ").upper()
+
     column = input("\nEnter ship column A-H: ").upper()
-    while column not in "ABCDEFGH":
-        print("\nPlease enter a valid column")
+    while len(column) == 0:
         column = input("\nEnter ship column A-H: ").upper()
-    return (int(row) - 1, letters_to_numbers[column])
+    while column not in "ABCDEFGH":
+        print('Please select a valid column A to H')
+        column = input("\nEnter ship column A-H: ").upper()
+    return int(row) - 1, letters_to_numbers[column]
 
 
 def count_hit_ships(board):
@@ -82,28 +88,32 @@ def count_hit_ships(board):
 create_ships(HIDDEN_BOARD)
 turns = 25
 while turns > 0:
-    print("⛴  WELCOME TO BATTLESHIPS ⛴\n")
-    print("YOU HAVE 25 GUESSES TO SINK 5 BATTLESHIPS\n")
-    print("Game Legend:")
-    print("▢ - blank space")
-    print("⨀ - missed shot")
-    print("♨ - hit ship\n")
     print_board(GUESS_BOARD)
     (row, column) = fire_shot()
-    if GUESS_BOARD[row][column] == "⨀ ":
-        print("\nYou've already guessed that \n")
+    if GUESS_BOARD[row][column] == "⊗":
+        print("\n------------------------------------------------")
+        print("You've already guessed that! Please guess again")
+        print("------------------------------------------------\n")
     elif HIDDEN_BOARD[row][column] == "♨":
-        print("\nGREAT SHOT! you sunk a battleship \n")
+        print("\n---------------------------------------")
+        print("GREAT SHOT! you sunk a battleship!")
+        print("---------------------------------------\n")
         GUESS_BOARD[row][column] = "♨"
         turns -= 1
     else:
-        print("\nAh unlucky, you missed! \n")
-        GUESS_BOARD[row][column] = "⨀"
+        print("\n------------------------")
+        print("Ah unlucky, you missed!")
+        print("------------------------\n")
+        GUESS_BOARD[row][column] = "⊗"
         turns -= 1
     if count_hit_ships(GUESS_BOARD) == 5:
+        print("\n------------------------------------------------------")
         print("\n CONGRATULATIONS! You've sunk all 5 battleships! \n")
+        print("------------------------------------------------------\n")
         break
     print("You have " + str(turns) + " turns remaining \n")
     if turns == 0:
+        print("\n--------------------------------------------")
         print("\n GAME OVER! you have 0 turns remaining \n")
+        print("--------------------------------------------\n")
         break
